33f801bb8f460f714294bbcae82523da03dcd5bc
Prince Kanu <kanuprince93@yahoo.com>
2023-08-01 00:16:53 -0500

    added more information about Pixel Haven and change CSS font and size

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(45.579591,9.164906); 
+  var myCenter = new google.maps.LatLng(30.217205, -97.762353); 
   var mapOptions = {center: myCenter, zoom: 13};
   var map = new google.maps.Map(mapCanvas,mapOptions);
   var marker = new google.maps.Marker({
